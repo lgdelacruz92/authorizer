1234915f23b64d242cb7b9e9e89fc2dce0c855c8
Lester <lgdelacruz92@gmail.com>
2024-08-31 12:27:58 -0400

    added compiler

---
+++ server.ts
@@ -0,0 +1,15 @@
+// src/server.ts
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
